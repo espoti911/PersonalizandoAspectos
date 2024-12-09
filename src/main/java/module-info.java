4bf07ad0a4e916c7f58dd5da0d8ce015bac73a1a
Francisco Manuel Gonzalez Martin <franciscomanuelgonzalezmartin@gmail.com>
2024-12-09 20:06:32 +0100

    Ejemplos de personalizacion de aspectos de componentes

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module es.ieslosmontecillos.uicontrolsample {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports es.ieslosmontecillos.uicontrolsample.checkboxsample;
+    exports es.ieslosmontecillos.uicontrolsample.tableviewsample;
+    exports es.ieslosmontecillos.uicontrolsample.tooltipsample;
+    exports es.ieslosmontecillos.uicontrolsample.listviewsample;
+
+}
